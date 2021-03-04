5ac8373702941ecfc2c2d5cd7b64dcd075c171ec
zane.chen <zane.chen@italki.com>
2021-03-04 19:26:58 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,208 +1,82 @@
-import Head from 'next/head'
-
+import 'antd/dist/antd.css'
+import { Form, Button, Input, Upload, Radio, message } from 'antd';
+import { useState } from 'react';
+import axios from 'axios'
 export default function Home() {
+  const [form] = Form.useForm();
+  const [way, setWay] = useState('1')
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Form
+        colon={false}
+        labelCol={{ span: 12 }}
+        wrapperCol={{ span: 12 }}
+        form={form}
+        onFinish={data => {
+          console.log(data)
+          const { desc, faxnum, name, phone, qq, way, file, address } = data;
+          let postData = new FormData();
+          postData.append('desc', desc);
+          postData.append('faxnum', faxnum)
+          postData.append('name', name)
+          postData.append('phone', phone)
+          postData.append('qq', qq)
+          postData.append('way', way)
+          postData.append('address', address)
+          postData.append('file', file[0])
+          axios({
+            url: 'http://42.192.42.107:3000/add-user',
+            method: 'post',
+            data: postData,
+          }).then(
+            res=>{
+              message.success('上传成功')
+            }
+          ).catch(error=>{
+            message.error('上传失败')
+          })
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+        }}
+        initialValues={{
+          way: way
+        }}
+      >
+        <Form.Item name='desc' label='问题描述' rules={[{ required: true }]}>
+          <Input.TextArea />
+        </Form.Item>
+        <Form.Item name='file' label='上传文件' rules={[{ required: true }]} valuePropName='files'>
+          <input type='file'></input>
+        </Form.Item>
+        <Form.Item name='name' label='公司名称' rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name='faxnum' label='税号' rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item name='way' label='预约方式' rules={[{ required: true }]}>
+          <Radio.Group options={[{ label: '远程', value: '1' }, { label: '上门', value: '2' }]}
+            onChange={e => {
+              e?.target?.value && setWay(e?.target?.value)
+            }}
+          />
+        </Form.Item>
+        {way === '1' && <Form.Item name='qq' label='QQ' rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>}
+        <Form.Item name='phone' label='电话号码' rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        {way === '2' && <Form.Item name='address' label='地址' rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>}
+        <Form.Item label=' '><Button type='primary' onClick={form.submit}>提交</Button></Form.Item>
+      </Form>
 
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
+       .container{
+         width:600px;
+         margin-top:40px
+       }
       `}</style>
     </div>
   )
